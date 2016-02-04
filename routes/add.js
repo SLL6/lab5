86c968b96e74b964d3308596608439db3263acc2
Scott Lim <sll009@ucsd.edu>
2016-02-04 19:59:07 +0000

    Lab 5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,14 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
+exports.addFriend = function(req, res) {   
+	var name = req.query.name;
+	var description = req.query.description;
+	
+	data['friends'].push( {"name":name,"description":description, imageURL: ""} );
+	
+	res.render('index', data);
+	console.log(data);
+
+
 	// Your code goes here
- }
+}
